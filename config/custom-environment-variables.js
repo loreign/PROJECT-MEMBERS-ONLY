b00322f90230dfc89ccc1a78b982de41c1ced2a2
loreign <69034427+loreign@users.noreply.github.com>
2020-10-09 16:53:47 +0300

    Add files via upload

---
+++ config/custom-environment-variables.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: 'ENV',
+  defaultFromEmail: 'DEFAULT_FROM_EMAIL',
+  logLevel: 'LOG_LEVEL',
+  port: 'PORT',
+  sessionSecret: 'SESSION_SECRET',
+  stripeAPIKey: 'STRIPE_API_KEY',
+}
---
+++ config/default.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: 'development',
+  defaultFromEmail: '"Chimera Staff" <info@chimeraarts.org>',
+  logLevel: 'DEBUG',
+  port: 3000,
+  sessionSecret: 'sekretstuffz',
+  stripeAPIKey: 'XXrG3naVp36XyGzdYycZasxc2S1Rk9go',
+}
